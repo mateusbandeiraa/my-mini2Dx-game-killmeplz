a5813e75d393ae6f73c84440ffa4c5e85c320f1e
Mateus Bandeira <mateusbandeiraa@gmail.com>
2019-04-02 15:54:17 -0300

    Insere menu

--- core/src/main/java/dev/mateusbandeira/killmeplz/MenuScreen.java
+++ core/src/main/java/dev/mateusbandeira/killmeplz/MenuScreen.java
@@ -2,34 +2,23 @@ package dev.mateusbandeira.killmeplz;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import org.mini2Dx.core.collisions.PointQuadTree;
-import org.mini2Dx.core.collisions.QuadTree;
-import org.mini2Dx.core.collisions.RegionQuadTree;
-import org.mini2Dx.core.engine.geom.CollisionPoint;
 import org.mini2Dx.core.game.GameContainer;
-import org.mini2Dx.core.geom.Rectangle;
 import org.mini2Dx.core.graphics.Graphics;
 import org.mini2Dx.core.screen.BasicGameScreen;
 import org.mini2Dx.core.screen.GameScreen;
 import org.mini2Dx.core.screen.ScreenManager;
 
-import com.badlogic.gdx.utils.Array;
 
-import entity.AmmoRefueler;
-import entity.Player;
 import org.mini2Dx.core.screen.transition.FadeInTransition;
 import org.mini2Dx.core.screen.transition.FadeOutTransition;
 
-public class IngameScreen extends BasicGameScreen {
-	public static final int ID = 1;
+public class MenuScreen extends BasicGameScreen {
+	public static final int ID = 2;
 	
-	private Player player;
-	private AmmoRefueler refueler1;
-	private static final String MESSAGE = "Tank reloaded!";
+	
 	@Override
 	public void initialise(GameContainer gc) {
-		player = new Player();
-		refueler1 = new AmmoRefueler();
+            
 
 	}
 
@@ -38,31 +27,22 @@ public class IngameScreen extends BasicGameScreen {
 		if (Gdx.input.isKeyPressed(Input.Keys.TAB)) {
                     screenManager.enterGameScreen(2, new FadeInTransition(), new FadeOutTransition());
                 }
-                player.update();
-		refueler1.update();
 
 	}
 
 	@Override
 	public void interpolate(GameContainer gc, float alpha) {
-		player.interpolate(alpha);
-		refueler1.interpolate(alpha);
+
 
 	}
 
 	@Override
 	public void render(GameContainer gc, Graphics g) {
-		refueler1.render(g);
-		player.render(g);
-		
-		if(refueler1.checkCollision(player))
-			g.drawString(MESSAGE, refueler1.getPoint().getX() - 32f, refueler1.getPoint().getY() + 48f);
-
+		g.drawString("Isso é um menu.", 32f, 32f);
 	}
 
 	@Override
 	public int getId() {
-		// TODO Auto-generated method stub
 		return this.ID;
 	}
 
