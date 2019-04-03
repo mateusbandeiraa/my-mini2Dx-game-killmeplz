079fc3dcb377d73f96f27c5cb06ce1d9da833c19
Mateus Bandeira <mateusbandeiraa@gmail.com>
2019-04-03 15:43:59 -0300

    Delta no Update
    Adiciona o parâmetro delta necessário no update.

---
+++ core/src/main/java/entity/IngameScreen.java
@@ -0,0 +1,70 @@
+package dev.mateusbandeira.killmeplz;
+
+import org.mini2Dx.core.game.GameContainer;
+import org.mini2Dx.core.graphics.Graphics;
+import org.mini2Dx.core.screen.BasicGameScreen;
+import org.mini2Dx.core.screen.GameScreen;
+import org.mini2Dx.core.screen.ScreenManager;
+import org.mini2Dx.core.screen.transition.FadeInTransition;
+import org.mini2Dx.core.screen.transition.FadeOutTransition;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+
+import entity.AmmoRefueler;
+import entity.Player;
+
+public class IngameScreen extends BasicGameScreen {
+	public static final int ID = 1;
+
+	private Player player;
+	private AmmoRefueler refueler1;
+	private static final String MESSAGE = "Tank reloaded!";
+
+	@Override
+	public void initialise(GameContainer gc) {
+		player = new Player();
+		refueler1 = new AmmoRefueler();
+
+	}
+
+	@Override
+	public void update(GameContainer gc, ScreenManager<? extends GameScreen> screenManager, float delta) {
+		// Verifica se o jogador pressionou TAB para abrir o menu.
+		if (Gdx.input.isKeyPressed(Input.Keys.TAB)) {
+			screenManager.enterGameScreen(2, new FadeOutTransition(), new FadeInTransition());
+		}
+		player.update(delta);
+		refueler1.update();
+
+	}
+
+	@Override
+	public void interpolate(GameContainer gc, float alpha) {
+		player.interpolate(alpha);
+		refueler1.interpolate(alpha);
+
+	}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) {
+		refueler1.render(g);
+		player.render(g);
+
+		// Verifica se existe colisão entre o jogador e o Refueler.
+		if (refueler1.checkCollision(player)) {
+			g.drawString(MESSAGE, refueler1.getPoint().getX() - 32f, refueler1.getPoint().getY() + 48f);
+		}
+
+	}
+
+	public Player getPlayer() {
+		return this.player;
+	}
+
+	@Override
+	public int getId() {
+		return IngameScreen.ID;
+	}
+
+}
