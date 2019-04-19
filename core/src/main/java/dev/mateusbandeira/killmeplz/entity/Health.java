7cb41f795ec89de18b75ef3b9d17ea50dc9ef38d
mateusbandeiraa <mateusbandeiraa@gmail.com>
2019-04-18 22:07:26 -0300

    Healthbar

---
+++ core/src/main/java/dev/mateusbandeira/killmeplz/entity/Health.java
@@ -0,0 +1,71 @@
+package dev.mateusbandeira.killmeplz.entity;
+
+import org.mini2Dx.core.game.GameContainer;
+import org.mini2Dx.core.graphics.Graphics;
+
+import com.badlogic.gdx.graphics.Color;
+
+public class Health extends GameObject {
+	private float maxHealth;
+	private float curHealth;
+	private GameObject target;
+
+	public Health(float maxHealth, float curHealth, GameObject target) {
+		super();
+		this.maxHealth = maxHealth;
+		this.curHealth = curHealth;
+		this.target = target;
+	}
+
+	@Override
+	public void behave(GameContainer gc, float delta) {
+		float x = this.target.getX();
+		float y = this.target.getY() + 32f + 8f;
+
+		this.setPosition(x, y);
+	}
+
+	@Override
+	public void render(Graphics g) {
+		g.setColor(Color.WHITE);
+		g.drawRect(getX(), getY(), 32f, 5f);
+		g.setColor(getPercentage() > 0.4f ? Color.GREEN : Color.RED);
+		g.fillRect(getX(), getY(), (32f * getPercentage()), 5f);
+	}
+
+	private float getPercentage() {
+		return curHealth / maxHealth;
+	}
+
+	public float getMaxHealth() {
+		return maxHealth;
+	}
+
+	public void setMaxHealth(float maxHealth) {
+		this.maxHealth = maxHealth;
+	}
+
+	public float getCurHealth() {
+		return curHealth;
+	}
+
+	public void setCurHealth(float curHealth) {
+		if(curHealth > maxHealth || curHealth < 0) {
+			return;
+		}
+		this.curHealth = curHealth;
+		System.out.println("curHealth: " + curHealth);
+	}
+	
+	public void changeHealth(float amount) {
+		if(curHealth + amount > maxHealth || curHealth + amount < 0) {
+			return;
+		}
+		curHealth += amount;
+	}
+
+	public GameObject getTarget() {
+		return target;
+	}
+
+}
--- core/src/main/java/dev/mateusbandeira/killmeplz/entity/Player.java
+++ core/src/main/java/dev/mateusbandeira/killmeplz/entity/Player.java
@@ -7,13 +7,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
 
-public class Player extends GameObject{
+public class Player extends GameObject {
 	private static final float DIMENSIONS = 32;
 	private static final TankColor INITIAL_COLOR = TankColor.YELLOW;
 	private TankColor color = INITIAL_COLOR;
+	private Health health;
 
 	public Player() {
 		super(32f, 8f, DIMENSIONS, DIMENSIONS, new Texture(INITIAL_COLOR.sprite));
+		health = new Health(10, 10, this);
 	}
 
 	public void behave(GameContainer gc, float delta) {
@@ -26,19 +28,27 @@ public class Player extends GameObject{
 				sprite.flip(true, false);
 			this.setPosition(this.getX() - 4f, this.getY());
 		}
+
+		if (Gdx.input.isKeyJustPressed(Keys.Q)) {
+			this.health.changeHealth(-1);
+		} else if (Gdx.input.isKeyJustPressed(Keys.W)) {
+			this.health.changeHealth(+1);
+		}
+		health.behave(gc, delta);
 	}
 
 	public void render(Graphics g) {
 		g.drawSprite(this.sprite);
+		health.render(g);
 	}
-	
+
 	public void changeColor(TankColor color) {
 		this.color = color;
 		this.sprite.setTexture(new Texture(this.color.sprite));
 	}
-	
+
 	public TankColor getColor() {
 		return this.color;
 	}
-	
+
 }
